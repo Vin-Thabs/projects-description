b1ccfb00b0307fbcbc896c8bb7a36c656b4b81a1
Thabiso Mahlaola <mahlaolathabiso@gmail.com>
2025-08-16 07:04:35 +0200

    vite depoyment

--- main.js
+++ main.js
@@ -7,33 +7,29 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    75, //field view
-    window.innerWidth / window.innerHeight,//aspect ratio
-    0.1, //near panes
-    1000 // far panes
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
 
-//camera position(x,y,z)
-camera.position.set(0,2,5);
-
-// 5 is the length of the axis
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-//grid helper(x,y) grid dimetions,
-const gridHelper = new THREE.GridHelper(10,50);
+camera.position.set(0, 2, 5);
+
+const gridHelper = new THREE.GridHelper(15, 50);
 scene.add(gridHelper);
 
-//material
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
-function animate(){
-    box.rotation.x += 0.01;
-    box.rotation.y +=   0.01;
+function animate(time) {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
     renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animate)
+renderer.setAnimationLoop(animate);
