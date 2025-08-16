3da12c9928911cbf7a3bc6dcd2b3219f8844bbbf
Thabiso Mahlaola <mahlaolathabiso@gmail.com>
2025-08-16 07:40:31 +0200

    static files serve

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/projects-description',
   build: {
     outDir: 'dist', // Output directory for build
     assetsDir: 'assets', // Where static assets like images will be placed
