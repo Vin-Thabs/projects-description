47e928abfac15c254a577a329404f7cc6eb33471
vin <vin@vin.vin.org>
2025-11-10 14:28:15 +0200

    Controls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -10,26 +11,48 @@ const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    2000
 );
+//camera position (x, y, z)
+camera.position.set(1000, 1000, 1000);
 
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
+//lights
 
-camera.position.set(0, 2, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft global light
+scene.add(ambientLight);
 
-const gridHelper = new THREE.GridHelper(15, 50);
-scene.add(gridHelper);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5,10, 7.5);
+scene.add(directionalLight);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00FF00});
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
+const pointLight = new THREE.PointLight(0xffffff, 0.8);
+pointLight.position.set(-5, 5, 5);
+scene.add(pointLight);
+
+//Orbit controls 
+
+const Orbit_control = new OrbitControls(camera, renderer.domElement);
+Orbit_control.enableDamping = true;
+// Orbit_control.target.set(0,0,0);
+Orbit_control.update();
+
+// load a 3D model
+const loader = new FBXLoader();
+
+loader.load( 'models/Gaming_Pc.fbx', function ( fbx ) {
+ // fbx.scale.setScalar(0.5); //Scale the model (Because its huge)
+  scene.add( fbx );
+
+}, undefined, function ( error ) {
+
+  console.error( error );
+
+} );
+
 
 function animate(time) {
-    box.rotation.x = time / 1000;
-    box.rotation.y = time / 1000;
-    renderer.render(scene, camera);
+  Orbit_control.update();
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
